b16437640071fb2b25a0534f7cec757326fadd45
MCANMCAN <can.tanriverdi01@gmail.com>
2022-03-23 23:19:44 +0300

    variables set for standart values .

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 from math import floor
 from playsound import playsound
 
-# ---------------------------- CONSTANTS ------------------------------- #
+# CONSTANTS #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -15,7 +15,7 @@ CHECK_MARK = "✔"
 CYCLE=0
 TIMER = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# TIMER RESET  # 
 def play() :
     playsound("notification.mp3")
 def reseted(): 
@@ -30,21 +30,20 @@ def started():
     CYCLE +=1
     print(CYCLE)
     #Timer devam etmeli .
-    if CYCLE == 5 : 
-        counter(4*60)  
+    if CYCLE == 9 : 
+        counter(LONG_BREAK_MIN*60)  
         canvas.itemconfigure(counter_label, fill=RED)
         title_label.configure(text="LONG REST",fg=RED)
     elif CYCLE !=1 and CYCLE % 2 == 0 : 
-        counter(1*60) 
+        counter(SHORT_BREAK_MIN*60) 
         canvas.itemconfigure(counter_label, fill=GREEN)
         title_label.configure(text="REST",fg=GREEN)
         mark_str = int(CYCLE/2)*CHECK_MARK
         check_mark.config(text=mark_str)
     elif CYCLE == 1 or CYCLE % 2 == 1 : 
-        counter(1*60) 
+        counter(WORK_MIN*60) 
         canvas.itemconfigure(counter_label, fill=PINK) 
         title_label.configure(text="CONCENTRATE",fg=PINK)    
-    #counter(25 * 60)
 # COUNTDOWN MECHANISM # 
 def counter(count) : 
     mn= floor(count/60)
@@ -61,13 +60,9 @@ def counter(count) :
       TIMER = window.after(1000 , counter , count - 1)
     elif count == 0:
       play()
-      print("hımm")
+      #print("hımm")
       started()
       
-          
-
-          
-
 # UI SETUP  #
 window = Tk()
 window.title("Promodoro")
@@ -76,16 +71,13 @@ canvas = Canvas(width=512,height=512)
 coffee = PhotoImage(file="hard-work.png")
 canvas.create_image(250,250, image=coffee)
 counter_label = canvas.create_text(170,270,text="25:00",fill=PINK, font=(FONT_NAME,35,"bold"))
-#counter(5)
 canvas.grid(column=1,row=1)
 start = Button(width=5,height=1,text="START",highlightthickness=0,font=(FONT_NAME,12,"bold"),bg=PINK,command=started)
 start.grid(row=0,column=0)
 reset = Button(width=5,height=1,text="RESET",highlightthickness=0,font=(FONT_NAME,12,"bold"),bg=PINK,command=reseted)
 reset.grid(row=0,column=3)
-
 title_label = Label(text="TIMER", font=(FONT_NAME, 35, "bold"),fg=GREEN)
 title_label.grid(row=0,column=1)
-
 check_mark= Label(font=(FONT_NAME, 15, "bold") ,  fg=GREEN )
 check_mark.grid(column=1,row=2)
 
